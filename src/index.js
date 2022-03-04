8e2d5d8a15c824be19db71303d8819ea78be81d1
Emma Perlman <emmajperlman@gmail.com>
2022-03-03 16:38:35 -0800

    commit tree f3110a410cb08a746ede67b32b86c78fb448b4b6

--- src/index.js
+++ src/index.js
@@ -12,4 +12,80 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  //Getting cards from server
+  fetch('http://localhost:3000/toys')
+  .then(res=>res.json())
+  .then(toys=>addToyCard(toys))
+
+  //Creating a card for each toy
+  function addToyCard(toyList){
+    toyList.forEach(toy=>makeToyCard(toy))
+  }
+//Creating a toy card
+  function makeToyCard(toy){   
+    const toyCard=document.createElement('div')
+    toyCard.className='card'
+
+    const name=document.createElement('h2')
+    name.textContent=`${toy.name}`
+    toyCard.appendChild(name)
+
+    const img=document.createElement('img')
+    img.src=`${toy.image}`
+    img.className='toy-avatar'
+    toyCard.appendChild(img)
+
+    const likes=document.createElement('p')
+    likes.textContent=`${toy.likes} Likes`
+    toyCard.appendChild(likes)
+
+    const likeButton=document.createElement('button')
+    likeButton.textContent='Like'
+    likeButton.id=`${toy.id}`
+    likeButton.className='like-btn'
+    toyCard.appendChild(likeButton)
+
+    //updating dom when like button is clicked
+    toyCard.querySelector(`button`).addEventListener('click',()=>{
+      toy.likes+=1
+      toyCard.querySelector('p').textContent=`${toy.likes} Likes`
+      updateLikes(toy)
+    })
+    document.querySelector('#toy-collection').appendChild(toyCard)
+  }
+    
+  //Collecting toy from form and posting to server
+  document.querySelector('.add-toy-form').addEventListener('submit',handleSubmit)
+  function handleSubmit(e){
+    e.preventDefault()
+    let toyObj={
+      name:e.target.name.value,
+      image:e.target.image.value,
+      likes:0
+    }
+    fetch('http://localhost:3000/toys',{
+      method: 'POST',
+      headers:{
+        'Content-Type':'application/json',
+        Accept:'application/json'
+      },
+      body:JSON.stringify(toyObj)
+    })
+    .then(res=>res.json())
+    .then(toy=>makeToyCard(toy)) //making the toy a card on dom
+  }
+  
+  function updateLikes(toyObj){
+    fetch(`http://localhost:3000/toys/${toyObj.id}`,{
+      method: 'PATCH',
+      headers:{
+        'Content-Type':'application/json',
+        Accept:'application/json'
+      },
+      body:JSON.stringify(toyObj)
+    })
+    .then(res=>res.json())
+     
+  }
+  
 });
